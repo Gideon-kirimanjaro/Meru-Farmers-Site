b4b1544dd0bab18d84793d37476dc7c6884c8fc9
Gideon Kirimanjaro <gideonkirimanjaro@gmail.com>
2022-11-24 05:51:32 +0300

    .env update

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,7 @@ import { useContext } from "react";
 import BlogContext from "../store/blog-data";
 const CONTENT_API_KEY = process.env.CONTENT_API_KEY;
 const GHOST_URL = process.env.GHOST_URL;
-const api =
-  "https://gideon-kamau.ghost.io/ghost/api/content/posts/?key=$15ba85399bb4588e4ddc2b8e1a&include=tags,authors,slug";
+
 export default function Home({ blogs }) {
   return (
     <div>
